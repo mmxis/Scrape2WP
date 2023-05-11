9882fc1c84792c29f4ba02818be851e95bc8ef88
mmxis <71505911+mmxis@users.noreply.github.com>
2023-05-11 10:16:53 +0200

    Update gptTranser.py

--- gptTranser.py
+++ gptTranser.py
@@ -43,6 +43,40 @@ def summarize_and_translate_text(text, source_language='en', target_language='de
 
     return translated_text
 
+# Funktion, um Text mithilfe der OpenAI API zusammenzufassen und zu übersetzen
+def summarize_and_translate_title(text, source_language='en', target_language='de'):
+    # Zusammenfassen
+    summary_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Please provide a similar version of the following text and make it plagiarism free: {text}",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.8,
+    )
+
+    if summary_response.choices:
+        summarized_text = summary_response.choices[0].text.strip()
+    else:
+        raise Exception('OpenAI API call failed')
+
+    # Funktion zum Übersetzen der vorherigen Funktion
+    translation_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Translate the following text from {source_language} to {target_language}: {summarized_text} keep names brands or organizations as it is in the original in the text.Make sure that the text fits good as a title on a news page and do not exceed 80 characters.",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.8,
+    )
+
+    if translation_response.choices:
+        translated_text = translation_response.choices[0].text.strip()
+    else:
+        raise Exception('OpenAI API call failed')
+
+    return translated_text
+
 # Zusammenfassen und Übersetzen für jede Zeile in den gewählten Spalten
 for idx, row in df.iterrows():
     # Für 'text' Spalte
@@ -52,7 +86,7 @@ for idx, row in df.iterrows():
 
     # Für 'title' Spalte
     original_title = row['title']
-    translated_summary_title = summarize_and_translate_text(original_title)
+    translated_summary_title = summarize_and_translate_title(original_title)
     df.at[idx, 'title'] = translated_summary_title
 
     print(f"Row {idx + 1}: Original Text = {original_text}")
@@ -65,3 +99,5 @@ for idx, row in df.iterrows():
 output_csv_file = 'modified_csv_file.csv'
 df.to_csv(output_csv_file, index=False)
 print(f"Modified CSV file saved as {output_csv_file}")
+
+
